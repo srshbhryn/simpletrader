31b78b2d59eccddc06cf35e033278289d0ebf7af
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2021-07-30 00:33:57 +0430

    improvment in settings and journals.

--- nobitex/views.py
+++ nobitex/views.py
@@ -1,17 +1,1 @@
 from django.shortcuts import HttpResponse
-
-from .tasks import collect_market_data
-from .models import Market
-# from .client import Client
-
-
-def test_celery(request):
-    # client = Client()
-    # print(client.get_orderbook('BTCIRT'))
-
-    # return HttpResponse(
-    #     str(client.get_orderbook('BTCIRT'))
-    # )
-    t = collect_market_data.delay()
-    # collect_market_data.delay(market.symbol, market.id)
-    return HttpResponse(':D')
