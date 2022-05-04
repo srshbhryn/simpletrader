20701849b0a06feb51ddc3f12983225066907811
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-05-04 22:51:25 +0430

    kucoin_index: added task for computing futures entropy

--- kucoin_index/tasks/index_computation_tasks.py
+++ kucoin_index/tasks/index_computation_tasks.py
@@ -53,5 +53,40 @@ def compute_spottrade_entropy(measure_id, time):
 
 
 @register_task(Type.futures_trade_entropy)
-def compute_futurestrade_entropy(related_id, period, price_rounding_factor):
-    log.info('CALLED futurestrade_entropy')
+def compute_futurestrade_entropy(measure_id, time):
+    time = datetime.datetime.fromisoformat(time)
+    measure = Measure.objects.get(pk=measure_id)
+    related_id = measure.related_id
+    period = measure.period
+    price_rounding_factor = measure.params['price_rounding_factor']
+    normal_price_rounding_factor = price_rounding_factor * FuturesContract.objects.get(pk=related_id).tick_size
+    values = FuturesTrade.objects.filter(
+        market_id=related_id,
+        time__lt=time,
+        time__gte=time-period,
+    ).annotate(rounded_price=ExpressionWrapper(Round(
+        F('price') / normal_price_rounding_factor,
+            output_field=FloatField(),
+        ),
+        output_field=FloatField(),
+    )).values('is_buyer_maker', 'rounded_price').annotate(
+        volume_sum=Sum('volume'),
+        is_buyer_maker_volume_sum=Sum('volume', filter=Q(is_buyer_maker=True)),
+        is_seller_maker_volume_sum=Sum('volume', filter=Q(is_buyer_maker=False)),
+    )
+    volume_sums = []
+    is_buyer_maker_volume_sums = []
+    is_seller_maker_volume_sums = []
+    for value in values:
+        volume_sums.append(value['volume_sum'])
+        is_buyer_maker_volume_sums.append(value['is_buyer_maker_volume_sum'])
+        is_seller_maker_volume_sums.append(value['is_seller_maker_volume_sum'])
+    Measurement.objects.create(
+        measure_id=measure_id,
+        time=time,
+        values={
+            'e':entropy(volume_sums),
+            'be':entropy(is_buyer_maker_volume_sums),
+            'se':entropy(is_seller_maker_volume_sums),
+        },
+    )
