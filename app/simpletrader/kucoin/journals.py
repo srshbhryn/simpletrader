d8d56b7a3d0dbbb18ab1699fa25b3ab2336c2e0f
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-07-08 20:51:26 +0430

    Kucoin: removed older trade models

--- app/simpletrader/kucoin/journals.py
+++ app/simpletrader/kucoin/journals.py
@@ -1,15 +1,5 @@
 from simpletrader.base.journals import Journal
-from simpletrader.kucoin.models import Trade, SpotTrade, FuturesTrade
-
-
-class SpotTradeJournal(Journal):
-    class Meta:
-        model = SpotTrade
-
-
-class FuturesTradeJournal(Journal):
-    class Meta:
-        model = FuturesTrade
+from simpletrader.kucoin.models import Trade
 
 
 class TradeJournal(Journal):
