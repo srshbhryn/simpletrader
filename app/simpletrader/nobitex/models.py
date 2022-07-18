d1d152227c0bea269cc581870b47bd04652435df
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-07-18 20:44:15 +0430

    Nobitex: Made Market models to use kucoin.mdoels.Asset

--- app/simpletrader/nobitex/models.py
+++ app/simpletrader/nobitex/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 
+from simpletrader.kucoin.models import Asset
+
 
 class Market(models.Model):
-    base_asset = models.CharField(max_length=8)
-    quote_asset = models.CharField(max_length=8)
+    base_asset = models.IntegerField(choices=Asset.choices)
+    quote_asset = models.IntegerField(choices=Asset.choices)
 
     @property
     def symbol(self):
-        return self.base_asset + self.quote_asset
+        return Asset._value2member_map_[self.base_asset].name.upper() + \
+            Asset._value2member_map_[self.quote_asset].name.upper()
 
     def __str__(self):
         return self.symbol
