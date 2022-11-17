30d535685ac9186cf0c0764cd406f0063b8ef1fe
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-11-17 13:57:14 +0330

    BookWatch: nobitex: improved error handling

--- bookwatch/bookwatch/clients/nobitex.py
+++ bookwatch/bookwatch/clients/nobitex.py
@@ -8,35 +8,32 @@ from bookwatch.markets import nobitex_markets
 from bookwatch.clients.redis import redis
 
 class Nobitex:
-    request = HTTPRequest(
-        url='http://api.nobitex.ir/v2/orderbook/all',
-        method='GET',
-        body=None,
-    )
     def __init__(self) -> None:
         self.client = AsyncHTTPClient()
         self.loop: tornado.ioloop.IOLoop = tornado.ioloop.IOLoop.current()
 
     async def run(self):
         while True:
-            start_time = self.loop.time()
-            try:
-                response: HTTPResponse  = await self.client.fetch(self.request)
-                body: dict = json.loads(response.body)
-                assert body['status'] == 'ok'
-                for market in nobitex_markets:
-                    symbol = market.symbol
-                    data = [str(body[symbol]['lastUpdate'])]
-                    data += body[symbol]['bids'][0]
-                    data += body[symbol]['asks'][0]
-                    data = ','.join(data)
-                    self.loop.add_callback(redis.set, name=market.id, value=data)
-                sleep_time = 1.05 - (
-                    self.loop.time() - start_time
-                )
-                if sleep_time <= 0:
-                    continue
-                await sleep(sleep_time)
-            except Exception as e:
-                print(e)
+            self.loop.add_callback(self.fetch_and_store)
+            await sleep(2)
 
+    async def fetch_and_store(self):
+        try:
+            response: HTTPResponse  = await AsyncHTTPClient().fetch(request=HTTPRequest(
+                url='http://api.nobitex.ir/v2/orderbook/all',
+                method='GET',
+                body=None,
+                connect_timeout=2,
+                request_timeout=2,
+            ), raise_error=False)
+            body: dict = json.loads(response.body)
+            assert body['status'] == 'ok'
+            for market in nobitex_markets:
+                symbol = market.symbol
+                data = [str(body[symbol]['lastUpdate'])]
+                data += body[symbol]['bids'][0]
+                data += body[symbol]['asks'][0]
+                data = ','.join(data)
+                self.loop.add_callback(redis.set, name=market.id, value=data)
+        except Exception as e:
+            print(e)
